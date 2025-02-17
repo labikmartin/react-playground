2ea436da94960dd59556a8300c5904ccb8447712
Martin Labik <martin.labik@eman.cz>
2025-02-17 10:54:40 +0100

    configuring path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(__dirname, 'src/components'),
+    },
+  },
 });
